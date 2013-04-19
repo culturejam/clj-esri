1710377d20b71a028dd814d438b34881f7d75367
Ticean Bennett <ticean@gmail.com>
2013-04-19 11:02:47 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-esri "0.1.0-SNAPSHOT"
+  :description "Esri ArcGIS service client"
+  :url "https://github.com/culturejam/clj-esri"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ test/clj_esri/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-esri.core-test
+  (:require [clojure.test :refer :all]
+            [clj-esri.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
