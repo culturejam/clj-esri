599157f781caacab00f933dc68636235beb2971b
Ticean Bennett <ticean@gmail.com>
2013-05-02 22:48:04 -0700

    Fixup the dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "https://github.com/culturejam/clj-esri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [
+                 [clj-json "0.5.3"]
+                 [org.clojars.tavisrudd/clj-apache-http "2.3.2-SNAPSHOT"]
+                 [org.clojure/clojure "1.5.1"]
+                 ])
