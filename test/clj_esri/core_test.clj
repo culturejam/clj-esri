422038245caadbcaebad7b323a25ec3e7c7afa54
Ticean Bennett <ticean@gmail.com>
2013-05-02 22:32:41 -0700

    Add some tests

--- test/clj_esri/core_test.clj
+++ test/clj_esri/core_test.clj
@@ -2,6 +2,54 @@
   (:require [clojure.test :refer :all]
             [clj-esri.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest build-service-endpoint-defaults-test
+  (testing "should build arcgis-online endpoint root"
+    (is (= "http://www.arcgis.com/sharing/rest"
+           (build-service-endpoint :arcgis-online))))
+  (testing "should build arcgis-server endpiont root"
+    (is (= "http://sampleserver1.arcgisonline.com/arcgis/rest/services"
+           (build-service-endpoint :arcgis-server)))))
+
+
+(deftest build-service-endpoint-custom-test
+  (with-arcgis-config "mytest.arcgis.com/sharing/rest"
+                      "mytest.arcgisonline.com/arcgis/rest/services"
+    (testing "should build arcgis-online endpoint root with custom url"
+      (is (= "http://mytest.arcgis.com/sharing/rest"
+             (build-service-endpoint :arcgis-online))))
+    (testing "should build arcgis-server endpiont root with custom url"
+      (is (= "http://mytest.arcgisonline.com/arcgis/rest/services"
+             (build-service-endpoint :arcgis-server))))))
+
+
+(deftest with-https-test
+  (with-https
+    (testing "should build arcgis-online endpoint root with https protocol"
+      (is (= "https://www.arcgis.com/sharing/rest"
+             (build-service-endpoint :arcgis-online))))
+    (testing "should build arcgis-server endpiont root with https protocol"
+      (is (= "https://sampleserver1.arcgisonline.com/arcgis/rest/services"
+             (build-service-endpoint :arcgis-server))))))
+
+
+
+
+;API endpoints
+(deftest expand-uri-test
+  (testing "should replace tokens with values provided in the map"
+    (is (= "sometest.com/beep" (expand-uri "sometest.com/::blip::"
+                                       {:blip "beep"}))))
+  (testing "should replace multiple tokens with values provided in the map"
+    (is (= "sometest.com/beep/bar" (expand-uri "sometest.com/::blip::/::foo::"
+                                           {:blip "beep" :foo "bar"}))))
+  (testing "should not replace tokens with no value in map"
+    (is (= "sometest.com/::blip::" (expand-uri "sometest.com/::blip::" {})))))
+
+
+;simulate concatenating the complete URI
+(deftest build-complete-endpoint-test
+  (testing "simulate building complete api endpoint"
+    (is (= "http://www.arcgis.com/sharing/rest/my/fake/endpoint"
+       (str (build-service-endpoint :arcgis-online)
+            (expand-uri "/my/::real::/endpoint" {:real "fake"}))))))
