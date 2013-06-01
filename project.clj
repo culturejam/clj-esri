e3d892598ba4143669eab5ae979797992631e6ca
Ticean Bennett <ticean@gmail.com>
2013-06-01 08:01:53 -0700

    Update dependencies style, no change

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/culturejam/clj-esri"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [clj-json "0.5.3"]
+  :dependencies [[clj-json "0.5.3"]
                  [org.clojars.tavisrudd/clj-apache-http "2.3.2-SNAPSHOT"]
-                 [org.clojure/clojure "1.5.1"]
-                 ])
+                 [org.clojure/clojure "1.5.1"]])
