0a1225a82ecec764eab880aa143ef9fe2ba319af
Ticean Bennett <ticean@gmail.com>
2013-06-11 16:22:01 -0700

    Release 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-esri "0.2.2"
+(defproject clj-esri "0.2.4"
   :description "Esri ArcGIS service client"
   :url "https://github.com/culturejam/clj-esri"
   :license {:name "Eclipse Public License"
